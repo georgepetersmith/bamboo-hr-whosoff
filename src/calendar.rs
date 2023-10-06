3bf4b0d4f4bff6e6afd1035593f6817a8c275d2a
George Smith <georges@tigereyeconsulting.com>
2023-10-06 19:10:33 +0100

    Separate models into file

---
+++ src/calendar.rs
@@ -0,0 +1,39 @@
+use serde::{Serialize, Deserialize};
+use chrono::NaiveDate;
+
+use crate::date_serializer;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Calendar {
+    #[serde(rename = "$value")]
+    pub items: Vec<Item>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub enum ItemType {
+    TimeOff,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Item {
+    pub r#type: ItemType,
+    pub request: Request,
+    pub employee: Employee,
+    #[serde(with = "date_serializer")]
+    pub start: NaiveDate,
+    #[serde(with = "date_serializer")]
+    pub end: NaiveDate,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Request {
+    pub id: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Employee {
+    pub id: u32,
+    #[serde(rename = "$value")]
+    pub name: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,11 @@
 use chrono::{NaiveDate, Utc};
 use reqwest::blocking::Client;
-use serde::{Deserialize, Serialize};
 use serde_xml_rs::from_str;
 use std::env;
+use crate::calendar::{Calendar, Item};
 
+mod calendar;
 mod date_serializer;
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Calendar {
-    #[serde(rename = "$value")]
-    items: Vec<Item>,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
-enum ItemType {
-    TimeOff,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Item {
-    r#type: ItemType,
-    request: Request,
-    employee: Employee,
-    #[serde(with = "date_serializer")]
-    start: NaiveDate,
-    #[serde(with = "date_serializer")]
-    end: NaiveDate,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct Request {
-    id: u32,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct Employee {
-    id: u32,
-    #[serde(rename = "$value")]
-    name: String,
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let today = Utc::now().naive_utc().date();
@@ -82,6 +48,8 @@ fn get_whos_off(date: NaiveDate) -> Result<Vec<Item>, Box<dyn std::error::Error>
 
 #[cfg(test)]
 mod tests {
+    use crate::calendar::{Calendar, Item, ItemType, Request, Employee};
+
     use super::*;
 
     #[test]
